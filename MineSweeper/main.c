5e8127e3589aed63c8f0702c34ffd689c9feeff7
DESKTOP-10AP76S <yundohyun050121@gmail.com>
2021-12-06 01:46:07 +0900

    change console size (62*40)

--- MineSweeper/main.c
+++ MineSweeper/main.c
@@ -114,7 +114,7 @@ void main() {
     srand(time(NULL));
 
     system("title MineSweeper");
-    system("mode con cols=62 lines=35");
+    system("mode con cols=62 lines=40");
 
     DWORD mode;
     WORD key;
@@ -135,17 +135,17 @@ void main() {
 
     data.x = data.y = data.dif = data.mine = data.cheat = -1;
 
-    gotoxy(17, 13);
+    gotoxy(17, 15);
     printf("지뢰찾기 레벨을 선택해주세요.");
-    gotoxy(19, 14);
-    printf("┌──────┬────────┬──────┐");
-    gotoxy(19, 15);
-    printf("│ Easy │ Normal │ Hard │");
     gotoxy(19, 16);
+    printf("┌──────┬────────┬──────┐");
+    gotoxy(19, 17);
+    printf("│ Easy │ Normal │ Hard │");
+    gotoxy(19, 18);
     printf("└──────┴────────┴──────┘");
-    gotoxy(10, 17);
+    gotoxy(10, 19);
     printf("클릭이 안된다면 리본 메뉴에서 우클릭을 하고");
-    gotoxy(7, 18);
+    gotoxy(7, 20);
     printf("속성에서 '빠른 편집 모드'와 '삽입모드'를 꺼주세요.");
     gotoxy(0, 0);
 
@@ -157,7 +157,7 @@ void main() {
             gotoxy(0, 0);
             data.cheat = 1;
             printf("Wow! You Fount Cheat!");
-        } else if (20 <= x && x <= 42 && 14 <= y && y <= 16) {
+        } else if (20 <= x && x <= 42 && 16 <= y && y <= 18) {
             if (20 <= x && x <= 26) data.dif = 1;
             else if (27 <= x && x <= 35) data.dif = 2;
             else data.dif = 3;
